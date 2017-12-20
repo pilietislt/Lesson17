436dced2d7201c11a00cfa7859d98a8f7c0b83e0
TomasJ <jegelevicius@gmail.com>
2017-12-20 19:46:48 +0200

    addet main.java

---
+++ src/main/java/lesson17/Main.java
@@ -0,0 +1,12 @@
+package lesson17;
+
+public class Main {
+    public static void main(String[] args){
+
+        Printer printer = new Printer();
+
+
+        printer.scnnFile("test.txt");
+
+    }
+}
